8b8680157bb08a7e0a61cbd78e3c884ac77fd505
Janosch Machowinski <janosch.machowinski@dfki.de>
2017-06-27 10:10:19 +0200

    fix unit test for boost > 1.59
    Namespace tt_detail was renamed to fpc_detail

--- unit_test/boost_check_close_vector.hh
+++ unit_test/boost_check_close_vector.hh
@@ -10,7 +10,7 @@
 
 
 #include <boost/test/floating_point_comparison.hpp>
-
+#include <boost/version.hpp>
 #include <Eigen/Core>
 
 #define MTK_CHECK_CLOSE(left, right, tol) BOOST_CHECK_CLOSE(left, right, 100*tol)
@@ -19,9 +19,16 @@
 namespace boost {
 
 namespace test_tools {
+    
 
 using unit_test::readonly_property;
-
+#if BOOST_VERSION > 105800
+using boost::test_tools::fpc_detail::fpt_abs;
+using boost::test_tools::fpc_detail::safe_fpt_division;
+#else
+using boost::test_tools::tt_detail::fpt_abs;
+using boost::test_tools::tt_detail::safe_fpt_division;
+#endif
 
 template<class ftype, int dim1, int dim2>
 class close_at_tolerance<Eigen::Matrix<ftype, dim1, dim2> >{
@@ -36,21 +43,21 @@ public:
 	template<typename ToleranceBaseType>
 	explicit    close_at_tolerance( percent_tolerance_t<ToleranceBaseType>  tolerance, 
 			floating_point_comparison_type          fpc_type = FPC_STRONG ) 
-	: p_fraction_tolerance( tt_detail::fpt_abs( static_cast<FPT>(0.01)*tolerance.m_value ) )
+	: p_fraction_tolerance( fpt_abs( static_cast<FPT>(0.01)*tolerance.m_value ) )
 	, p_strong_or_weak( fpc_type ==  FPC_STRONG )
 	{}
 	template<typename ToleranceBaseType>
 	explicit    close_at_tolerance( fraction_tolerance_t<ToleranceBaseType> tolerance, 
 			floating_point_comparison_type          fpc_type = FPC_STRONG ) 
-	: p_fraction_tolerance( tt_detail::fpt_abs( tolerance.m_value ) )
+	: p_fraction_tolerance( fpt_abs( tolerance.m_value ) )
 	, p_strong_or_weak( fpc_type ==  FPC_STRONG )
 	{}
 	
 	bool        operator()( comp_type left, comp_type right ) const
 	{
 		FPT diff = ( left - right ).norm();
-		FPT d1   = tt_detail::safe_fpt_division( diff, right.norm() );
-		FPT d2   = tt_detail::safe_fpt_division( diff, left.norm() );
+		FPT d1   = safe_fpt_division( diff, right.norm() );
+		FPT d2   = safe_fpt_division( diff, left.norm() );
 		
 		return p_strong_or_weak 
 		? (d1 <= p_fraction_tolerance && d2 <= p_fraction_tolerance) 
